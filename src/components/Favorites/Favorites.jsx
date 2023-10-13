dc49b97bf375b932792e25da72b6a7f95832a886
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2023-10-13 20:36:00 +0300

    update

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,48 +1,48 @@
-import Catalog from "components/Catalog/Catalog";
-import { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+// import Catalog from "components/Catalog/Catalog";
+// import { useEffect, useState } from "react";
+// import { useSelector } from "react-redux";
 
-import { getFavorite } from "Redux/selectors";
+// import { getFavorite } from "Redux/selectors";
 
-function Favorites() {
-    const [card, setCard] = useState([])
+// function Favorites() {
+//     const [card, setCard] = useState([])
     
-    const favorite = useSelector(getFavorite);
-    const dispatch = useDispatch();
+//     const favorite = useSelector(getFavorite);
+//     // const dispatch = useDispatch();
     
-    console.log(favorite)
+//     console.log(favorite)
 
-    useEffect(() => {
-        dispatch(setCard(favorite))
-        // const arr = Object.entries(favorite)
+//     useEffect(() => {
+        
+//         const arr = Object.entries(favorite)
     
-        // if(arr.length) {
-        //     const res = arr.map(item => {
-        //         return {
-        //             id: item[0],
-        //             ...item[1]
-        //         }
-        //     })
-        //     console.log('res', res)
+//         if(arr.length) {
+//             const res = arr.map(item => {
+//                 return {
+//                     id: item[0],
+//                     ...item[1]
+//                 }
+//             })
+//             console.log('res', res)
 
-        //     setCard(res)
-        // }
+//             setCard(res)
+//         }
 
-    }, [])
+//     }, [])
 
-return (
-    <>
-    {card.length 
-    ? <Catalog card={card}/>
-    : <h2> No cards </h2> 
-    }
+// return (
+//     <>
+//     {card.length 
+//     ? <Catalog card={card}/>
+//     : <h2> No cards </h2> 
+//     }
     
-    </>
-)
+//     </>
+// )
     
    
-}
+// }
 
-export default Favorites;
+// export default Favorites;
 
 
